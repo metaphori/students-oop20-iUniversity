e5fd66968d02bbabcfc9edc937d73bf879577454
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-03-19 23:04:05 +0100

    Add main classes

---
+++ src/main/java/iuniversity/IUniversity.java
@@ -0,0 +1,14 @@
+package iuniversity;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class IUniversity extends Application {
+
+    public static void main(final String[] args) {
+    }
+
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+    }
+}
---
+++ src/main/java/iuniversity/Launcher.java
@@ -0,0 +1,11 @@
+package iuniversity;
+
+public final class Launcher {
+
+    private Launcher() {
+    }
+
+    public static void main(final String[] args) {
+        IUniversity.main(args);
+    }
+}
