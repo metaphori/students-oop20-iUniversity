6e3e7661ac72c0e87e738cbcf3fae12d80dab1d7
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-03-19 22:41:54 +0100

    Add project files

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+	java
+	application 
+	//id("org.openjfx.javafxplugin") version "0.0.9"
+}
+
+repositories {
+	mavenCentral()
+}
+
+//javafx {
+//	version = "15"
+//    modules("javafx.controls", "javafx.fxml")
+//}
+
+dependencies {
+	//implementation("com.calendarfx:view:11.8.3")
+	//implementation ("org.kordamp.bootstrapfx:bootstrapfx-core:0.4.0")
+}
