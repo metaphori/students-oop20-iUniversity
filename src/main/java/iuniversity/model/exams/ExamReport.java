33e2876fdf2c689ebb710cdd652814c5bf463282
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-03-26 00:38:35 +0100

    Add ExamReport interface and implementation

--- src/main/java/iuniversity/model/exams/ExamReport.java
+++ src/main/java/iuniversity/model/exams/ExamReport.java
@@ -1,5 +1,26 @@
 package iuniversity.model.exams;
 
+import iuniversity.model.didactics.Course;
+import iuniversity.model.user.Student;
+
 public interface ExamReport {
+
+    /**
+     * 
+     * @return the course to witch the evaluation is referred.
+     */
+    Course getCourse();
+
+    /**
+     * 
+     * @return the student to witch the evaluation is referred.
+     */
+    Student getStudent();
+
+    /**
+     * 
+     * @return the result.
+     */
+    ExamResult getResult();
 
 }
--- src/main/java/iuniversity/model/exams/ExamReportImpl.java
+++ src/main/java/iuniversity/model/exams/ExamReportImpl.java
@@ -1,7 +1,35 @@
 package iuniversity.model.exams;
 
+import iuniversity.model.didactics.Course;
+import iuniversity.model.user.Student;
+
 public final class ExamReportImpl implements ExamReport {
 
+    private final Course course;
 
+    private final Student student;
+
+    private final ExamResult result;
+
+    public ExamReportImpl(final Course course, final Student student, final ExamResult result) {
+        this.course = course;
+        this.student = student;
+        this.result = result;
+    }
+
+    @Override
+    public Course getCourse() {
+        return this.course;
+    }
+
+    @Override
+    public Student getStudent() {
+        return this.student;
+    }
+
+    @Override
+    public ExamResult getResult() {
+        return this.result;
+    }
 
 }
