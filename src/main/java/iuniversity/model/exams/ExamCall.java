99aa18d53b41fe3eced620b09dbdd8f9740203a9
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-03-26 00:09:23 +0100

    Add ExamCall interface methods

--- src/main/java/iuniversity/model/exams/ExamCall.java
+++ src/main/java/iuniversity/model/exams/ExamCall.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import java.util.Set;
 
 import iuniversity.model.didactics.AcademicYear;
+import iuniversity.model.didactics.Course;
 import iuniversity.model.user.Student;
 import iuniversity.model.user.Teacher;
 
@@ -17,15 +18,15 @@ public interface ExamCall {
      */
     enum CallStatus {
 
-        /**
+        /*
          * Exam call is created but not visible to students.
          */
-        CREATED,
-        /**
+        //CREATED,
+        /*
          * Exam call is created but and visible to students. Students can't register
          * yet.
          */
-        PUBLISHED,
+        //PUBLISHED,
         /**
          * Exam call is published and students can register to the call.
          */
@@ -40,18 +41,88 @@ public interface ExamCall {
         ORAL, WRITTEN, PRACTICAL;
     }
 
+    /**
+     * 
+     * @return the teacher's who published the call
+     */
     Teacher getTeacher();
 
+    /**
+     * 
+     * @return the ExamCall course
+     */
+    Course getCourse();
+
+    /**
+     * 
+     * @return the Academic year to witch the call is referenced
+     */
     AcademicYear getAcademicYear();
 
+    /**
+     * 
+     * @return the set of student's that registered to the call
+     */
     Set<Student> getRegisteredStudents();
 
+    /**
+     * 
+     * @return the start Date and time of the call
+     */
     LocalDateTime getStart();
 
+    /**
+     * 
+     * @return the {@link ExamType} of the call
+     */
     ExamType getExamType();
 
+    /**
+     * 
+     * @return the status of the call
+     */
     CallStatus getStatus();
 
+    /**
+     * 
+     * @return an optional with is filled with the maximum number of student that
+     *         register to the call
+     */
     Optional<Integer> maxStudents();
+
+    /**
+     * Sets the Exam Call start.
+     * 
+     * @param callStart
+     */
+    void setStart(LocalDateTime callStart);
+
+    /**
+     * Sets the the Exam call type. See {@link ExamType}
+     * 
+     * @param examType
+     */
+    void setExamType(ExamType examType);
+
+    /**
+     * Add student to the Exam call list.
+     * 
+     * @param student
+     */
+    void registerStudent(Student student);
+
+    /**
+     * Sets the maximum number of students that can register to the exam call.
+     * 
+     * @param maxStudents
+     */
+    void setMaximumStudents(int maxStudents);
+
+    /**
+     * Set the academic year of the exam call.
+     * 
+     * @param academicYear
+     */
+    void setAcademicYear(AcademicYear academicYear);
 
 }
