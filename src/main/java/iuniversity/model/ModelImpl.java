f09471a1dca634c510403d18d34fbf65ca5f8be3
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-03-27 01:05:33 +0100

    Add model implementation

---
+++ src/main/java/iuniversity/model/ModelImpl.java
@@ -0,0 +1,19 @@
+package iuniversity.model;
+
+import iuniversity.model.user.User;
+
+public class ModelImpl implements Model {
+
+    @Override
+    public User getLoggedUser() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setCurrentUser(User user) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
