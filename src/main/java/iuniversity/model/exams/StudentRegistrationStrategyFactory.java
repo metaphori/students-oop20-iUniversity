fa0024e8da2c871bac8f4c99e7772e3dbc57c9c8
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-03-28 12:32:31 +0200

    Add a javadoc comment

--- src/main/java/iuniversity/model/exams/StudentRegistrationStrategyFactory.java
+++ src/main/java/iuniversity/model/exams/StudentRegistrationStrategyFactory.java
@@ -2,10 +2,22 @@ package iuniversity.model.exams;
 
 public interface StudentRegistrationStrategyFactory {
 
+    /**
+     * 
+     * @return a strategy where the student is added ad the end of an exam call list.
+     */
     StudentRegistrationStrategy atTheEndOfList();
 
+    /**
+     * 
+     * @return a strategy where the student is added ad the top of an exam call list.
+     */
     StudentRegistrationStrategy atTheTopOfList();
 
+    /**
+     * 
+     * @return a strategy where the students are registered in alphabetical order.
+     */
     StudentRegistrationStrategy alfabeticalOrder();
 
 }
