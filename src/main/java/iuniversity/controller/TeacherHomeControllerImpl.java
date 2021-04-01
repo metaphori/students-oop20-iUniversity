e04805ed4d82f6a5a491131c91f3d9d5c1df2e62
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-04-01 19:36:09 +0200

    Add TeacherHomeController

---
+++ src/main/java/iuniversity/controller/TeacherHomeControllerImpl.java
@@ -0,0 +1,90 @@
+package iuniversity.controller;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import iuniversity.model.exams.ExamCall;
+import iuniversity.model.exams.ExamCall.CallStatus;
+import iuniversity.model.exams.ExamReport;
+import iuniversity.model.exams.ExamResult.ExamResultType;
+import iuniversity.model.user.Teacher;
+import iuniversity.model.user.User;
+import iuniversity.view.home.TeacherHomeView;
+
+public class TeacherHomeControllerImpl extends AbstractController implements TeacherHomeController {
+
+    private void checkUser() {
+        final Optional<User> loggedUser = this.getModel().getLoggedUser();
+        if (loggedUser.isEmpty()) {
+            throw new IllegalStateException("No user logged");
+        } else if (!(loggedUser.get() instanceof Teacher)) {
+            throw new IllegalStateException("User must be a teacher");
+        }
+    }
+
+    private Teacher getLoggedTeacher() {
+        return (Teacher) this.getModel().getLoggedUser().get();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void displayTeacherInfo() {
+        this.checkUser();
+        final Teacher teacher = getLoggedTeacher();
+        final TeacherHomeView view = (TeacherHomeView) this.getView();
+        view.setFirstName(teacher.getName());
+        view.setFirstName(teacher.getLastName());
+        view.setRegistrationNumber(teacher.getRegistrationNumber());
+    }
+
+    private Stream<ExamReport> getTeacherExamReportStream() {
+        return this.getModel().getExamManager().getExamReports().stream()
+                .filter(e -> getLoggedTeacher().getCourses().contains(e.getCourse()));
+    }
+
+    private Stream<ExamReport> getTeacherExamReportStreamByResultType(final ExamResultType examResultType) {
+        return getTeacherExamReportStream().filter(e -> e.getResult().getResultType() == examResultType);
+    }
+
+    private Stream<ExamCall> getTeacherExamCallsStream() {
+        return this.getModel().getExamManager().getExamCalls().stream()
+                .filter(e -> e.getTeacher().equals(getLoggedTeacher()));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void displayTeacherStatistics() {
+        this.checkUser();
+        final TeacherHomeView view = (TeacherHomeView) this.getView();
+        view.setPromotedStudents((int) getTeacherExamReportStreamByResultType(ExamResultType.SUCCEDED).count());
+        view.setFailedStudents((int) getTeacherExamReportStreamByResultType(ExamResultType.FAILED).count());
+        view.setWithdrawnStudents((int) getTeacherExamReportStreamByResultType(ExamResultType.WITHDRAWN).count());
+        view.setCumLaudeStudents((int) getTeacherExamReportStreamByResultType(ExamResultType.SUCCEDED)
+                .filter(e -> e.getResult().cumLaude()).count());
+        view.setMeanResults(getTeacherExamReportStream().map(e -> e.getResult()).filter(r -> r.getResult().isPresent())
+                .mapToDouble(r -> r.getResult().get()).average().orElse(0));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void removeExamCall(final ExamCall examCall) {
+        this.getModel().getExamManager().removeExamCall(examCall);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void displayOpenExamCalls() {
+        ((TeacherHomeView) this.getView()).setOpenExamCalls(
+                getTeacherExamCallsStream().filter(e -> e.getStatus() == CallStatus.OPEN).collect(Collectors.toSet()));
+    }
+
+}
