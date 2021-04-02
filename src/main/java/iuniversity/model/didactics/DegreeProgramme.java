05517b68d0a0d3bab82857e2444c7753b8856956
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-04-02 16:19:21 +0200

    Add get course method

--- src/main/java/iuniversity/model/didactics/DegreeProgramme.java
+++ src/main/java/iuniversity/model/didactics/DegreeProgramme.java
@@ -1,10 +1,14 @@
 package iuniversity.model.didactics;
 
+import java.util.Set;
+
 public interface DegreeProgramme {
 
     String getName();
     
     DegreeType getType();
+    
+    Set<Course> getCourses();
     
     public enum DegreeType {
         BACHELOR, MASTER
