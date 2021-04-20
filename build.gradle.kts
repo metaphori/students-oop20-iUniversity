9dff7413d5c6e1a8c61b8a0f09789fe94387a82e
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-04-20 19:41:15 +0200

    Use part of APICe-at-DISI build.gradle.kts for javafx applications

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,25 +2,37 @@ plugins {
 	java
 	application 
 	id("org.openjfx.javafxplugin") version "0.0.9"
+	
+	/*
+     * Adds tasks to export a runnable jar.
+     * In order to create it, launch the "shadowJar" task.
+     * The runnable jar will be found in build/libs/projectname-all.jar
+     */
+    id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 repositories {
 	mavenCentral()
 }
 
-javafx {
-	version = "15"
-    modules("javafx.controls", "javafx.fxml")
-}
+val javaFXModules = listOf(
+    "base",
+    "controls",
+    "fxml",
+    "swing",
+    "graphics"
+)
 
-application.mainClassName = "iuniversity.Launcher"
-
+val supportedPlatforms = listOf("linux", "mac", "win") // All required for OOP
+val javaFxVersion = "15.0.1"
 dependencies {
-	//implementation("com.calendarfx:view:11.8.3")
-	//implementation ("org.kordamp.bootstrapfx:bootstrapfx-core:0.4.0")
-	//testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
-    //testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
-    
+
+	// JavaFX: comment out if you do not need them
+    for (platform in supportedPlatforms) {
+        for (module in javaFXModules) {
+            implementation("org.openjfx:javafx-$module:$javaFxVersion:$platform")
+        }
+    }
     testRuntime("org.junit.jupiter:junit-jupiter-engine:5.5.2")
     testRuntime("org.junit.platform:junit-platform-runner:1.5.2")
     
@@ -35,4 +47,31 @@ dependencies {
 	implementation("com.google.guava:guava:30.1.1-jre")
 	
     
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Test> {
+    // Enables JUnit 5 Jupiter module
+    useJUnitPlatform()
+}
+
+application {
+    // Define the main class for the application
+    mainClass.set("iuniversity.Launcher")
+
+    /*
+     * mainClassName was deprecated by Gradle, but it is still required by John Engelman's Shadow plugin.
+     * A pull request with a fix was already merged, but it hasn't been released yet;
+     * see https://github.com/johnrengelman/shadow/issues/609 and https://github.com/johnrengelman/shadow/pull/612
+     */
+    @Suppress("DEPRECATION")
+    mainClassName = mainClass.get()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
